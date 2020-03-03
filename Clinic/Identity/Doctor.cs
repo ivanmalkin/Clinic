13c04eb0c7a704c12adb95b8108ca4df0e6b674b
ivanmalkin <Ivan-malkin1999@yandex.ru>
2020-03-03 09:36:15 +0300

    Изменение пользователей, наследование от ApplicationUser

--- Clinic/Identity/Doctor.cs
+++ Clinic/Identity/Doctor.cs
@@ -1,7 +1,16 @@
-﻿namespace Clinic.Identity
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Clinic.Identity
 {
     public class Doctor : ApplicationUser
     {
+        [Key]
+        [ForeignKey("ApplicationUser")]
+        public string DoctorId { get; set; }
+
         public int Experience { get; set; }
+
+        public virtual ApplicationUser ApplicationUser { get; set; }
     }
 }
--- Clinic/Identity/Patient.cs
+++ Clinic/Identity/Patient.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Clinic.Identity
 {
@@ -10,7 +12,13 @@ namespace Clinic.Identity
 
     public class Patient : ApplicationUser
     {
+        [Key]
+        [ForeignKey("ApplicationUser")]
+        public string PatientId { get; set; }
+
         public DateTime BirthDate { get; set; }
         public Gender Gender { get; set; }
+
+        public virtual ApplicationUser ApplicationUser { get; set; }
     }
 }
